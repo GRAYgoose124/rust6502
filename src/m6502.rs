b2eb5b0485f98de85ccea196efc674fcd4d6a7bc
Grayson Miller <grayson.miller124@gmail.com>
2022-01-27 21:37:17 -0700

    program string to ram
    mend

--- src/m6502.rs
+++ src/m6502.rs
@@ -1,3 +1,8 @@
+extern crate itertools;
+extern crate hex;
+
+use itertools::Itertools;
+use hex::decode;
 use bitmatch::bitmatch;
 
 
@@ -8,11 +13,13 @@ pub struct M6502 {
     pub x: u8,
     pub y: u8,
     pub sr: u8, // [NV-BDIZC]
-    pub sp: u8 
+    pub sp: u8,
+
+    pub ram: [u8; 256]
 }
 
 
-// https://llx.com/Neil/a2/opcodes.html
+/*/ https://llx.com/Neil/a2/opcodes.html
 #[derive(Debug)]
 pub enum Instr {
     BRK, JSR, RTI, RTS,
@@ -31,91 +38,169 @@ pub enum Instr {
     PHP, PLP, PHA, PLA, DEY, TAY, INY, INX,
     CLC, SEC, CLI, SEI, TYA, CLV, CLD, SED,
     TXA, TXS, TAX, TSX, DEX, NOP
-}
+}*/
 
+impl M6502 {
+    // Instructions
+    // aaabbbcc - cc = 01
+    pub fn ORA(&self, mode: u8) {}
+    pub fn AND(&self, mode: u8) {}
+    pub fn EOR(&self, mode: u8) {}
+    pub fn ADC(&self, mode: u8) {}
+    pub fn STA(&self, mode: u8) {}
+    pub fn LDA(&self, mode: u8) {}
+    pub fn CMP(&self, mode: u8) {}
+    pub fn SBC(&self, mode: u8) {}
+
+    // cc = 10
+    pub fn ASL(&self, mode: u8) {}
+    pub fn ROL(&self, mode: u8) {}
+    pub fn LSR(&self, mode: u8) {}
+    pub fn ROR(&self, mode: u8) {}
+    pub fn STX(&self, mode: u8) {}
+    pub fn LDX(&self, mode: u8) {}
+    pub fn DEC(&self, mode: u8) {}
+    pub fn INC(&self, mode: u8) {}
+
+    // cc = 00
+    pub fn BIT(&self, mode: u8) {}
+    pub fn JMP(&self, mode: u8) {}
+    pub fn AJMP(&self, mode: u8) {}
+    pub fn STY(&self, mode: u8) {}
+    pub fn LDY(&self, mode: u8) {}
+    pub fn CPY(&self, mode: u8) {}
+    pub fn CPX(&self, mode: u8) {}
+
+    // xxy10000; branches x=neg,over,carry zero; cmp(x, y)
+    pub fn BPL(&self) {}
+    pub fn BMI(&self) {}
+    pub fn BVC(&self) {}
+    pub fn BVS(&self) {}
+    pub fn BCC(&self) {}
+    pub fn BCS(&self) {}
+    pub fn BNE(&self) {}
+    pub fn BEQ(&self) {}
+
+    // no pattern
+    pub fn BRK(&self) {}
+    pub fn JSR(&self) {}
+    pub fn RTI(&self) {}
+    pub fn RTS(&self) {}
+
+    pub fn PHP(&self) {}
+    pub fn PLP(&self) {}
+    pub fn PHA(&self) {}
+    pub fn PLA(&self) {}
+    pub fn DEY(&self) {}
+    pub fn TAY(&self) {}
+    pub fn INY(&self) {}
+    pub fn INX(&self) {}
+
+    pub fn CLC(&self) {}
+    pub fn SEC(&self) {}
+    pub fn CLI(&self) {}
+    pub fn SEI(&self) {}
+    pub fn TYA(&self) {}
+    pub fn CLV(&self) {}
+    pub fn CLD(&self) {}
+    pub fn SED(&self) {}
+
+    pub fn TXA(&self) {}
+    pub fn TXS(&self) {}
+    pub fn TAX(&self) {}
+    pub fn TSX(&self) {}
+    pub fn DEX(&self) {}
+    pub fn NOP(&self) {}
+}
 
 impl M6502 {
     #[bitmatch]
-    pub fn match_instr(&self, instr: u8) -> Instr {
-        use crate::m6502::Instr::*;
-
+    fn match_instr(&self, instr: u8) {
         #[bitmatch]
         match instr {
-            "00000000" => BRK,
-            "00100000" => JSR, // absolute JSR
-            "01000000" => RTI,
-            "01100000" => RTS,
+            "00000000" => self.BRK(),
+            "00100000" => self.JSR(), // absolute JSR
+            "01000000" => self.RTI(),
+            "01100000" => self.RTS(),
             // cc = 01
             "ooommm01" => match o {
-                0x00 => ORA(m),   
-                0x01 => AND(m), 
-                0x02 => EOR(m), 
-                0x03 => ADC(m), 
-                0x04 => STA(m), 
-                0x05 => LDA(m),  
-                0x06 => CMP(m),
-                0x07 => SBC(m),
-                _ => NOP,
+                0x00 => self.ORA(m),   
+                0x01 => self.AND(m), 
+                0x02 => self.EOR(m), 
+                0x03 => self.ADC(m), 
+                0x04 => self.STA(m), 
+                0x05 => self.LDA(m),  
+                0x06 => self.CMP(m),
+                0x07 => self.SBC(m),
+                _ => self.NOP(),
             },
             // cc = 10
             "ooommm10" => match o {
-                0x00 => ASL(m),   
-                0x01 => ROL(m), 
-                0x02 => LSR(m), 
-                0x03 => ROR(m), 
-                0x04 => STX(m), 
-                0x05 => LDX(m),  
-                0x06 => DEC(m),
-                0x07 => INC(m),
-                _ => NOP
+                0x00 => self.ASL(m),   
+                0x01 => self.ROL(m), 
+                0x02 => self.LSR(m), 
+                0x03 => self.ROR(m), 
+                0x04 => self.STX(m), 
+                0x05 => self.LDX(m),  
+                0x06 => self.DEC(m),
+                0x07 => self.INC(m),
+                _ => self.NOP()
             },
             // cc = 00
             "ooommm00" => match o {
-                0x00 => BIT(m),   
-                0x01 => JMP(m), 
-                0x02 => AJMP(m), 
-                0x03 => STY(m), 
-                0x04 => LDY(m), 
-                0x05 => CPY(m),  
-                0x06 => CPX(m),
-                _ => NOP
+                0x00 => self.BIT(m),   
+                0x01 => self.JMP(m), 
+                0x02 => self.AJMP(m), 
+                0x03 => self.STY(m), 
+                0x04 => self.LDY(m), 
+                0x05 => self.CPY(m),  
+                0x06 => self.CPX(m),
+                _ => self.NOP()
             },
             // conditional jumps = xxy10000
-            "xxy10000" => match x { 
-                // [!] Assumes all cond jumps are the same.
-                // Negative
-                0x0 => if self.sr & 0b10000000 == y && y == 0 { BPL } else { BMI },
-                // Overflow
-                0x1 => if self.sr & 0b01000000 == y && y == 0 { BVC } else { BVS },
-                // Carry
-                0x2 => if self.sr & 0b00000001 == y && y == 0 { BCC } else { BCS },
-                // Zero
-                0x3 => if self.sr & 0b00000010 == y && y == 0 { BNE } else { BEQ },
-                _ => NOP
-            },
-            "00001000" => PHP, 
-            "00101000" => PLP, 
-            "01001000" => PHA, 
-            "01101000" => PLA,
-            "10001000" => DEY, 
-            "10101000" => TAY, 
-            "01001100" => INY, 
-            "11101000" => INX,
-            "00011000" => CLC,
-            "00111000" => SEC,
-            "01011000" => CLI,
-            "01111000" => SEI,
-            "10011000" => TYA,
-            "10111000" => CLV,
-            "11011000" => CLD,
-            "11111000" => SED,
-            "10001010" => TXA,
-            "10011010" => TXS,
-            "10101010" => TAX,
-            "10111010" => TSX,
-            "11001010" => DEX,
-            "11101010" => NOP,
-            _ => NOP,
+            "00010000" => self.BPL(),
+            "00110000" => self.BMI(),
+            "01010000" => self.BVC(),
+            "01110000" => self.BVS(),
+            "10010000" => self.BCC(),
+            "10110000" => self.BCS(),
+            "11010000" => self.BNE(),
+            "11110000" => self.BEQ(),
+            // no pattern
+            "00001000" => self.PHP(), 
+            "00101000" => self.PLP(), 
+            "01001000" => self.PHA(), 
+            "01101000" => self.PLA(),
+            "10001000" => self.DEY(), 
+            "10101000" => self.TAY(), 
+            "01001100" => self.INY(), 
+            "11101000" => self.INX(),
+            "00011000" => self.CLC(),
+            "00111000" => self.SEC(),
+            "01011000" => self.CLI(),
+            "01111000" => self.SEI(),
+            "10011000" => self.TYA(),
+            "10111000" => self.CLV(),
+            "11011000" => self.CLD(),
+            "11111000" => self.SED(),
+            "10001010" => self.TXA(),
+            "10011010" => self.TXS(),
+            "10101010" => self.TAX(),
+            "10111010" => self.TSX(),
+            "11001010" => self.DEX(),
+            "11101010" => self.NOP(),
+            _ => self.NOP(),
+        };
+    }
+
+    pub fn run(&mut self, prog: &str) {
+        let i = 0;
+
+        self.ram[0..prog.len()/2].copy_from_slice(&decode(prog).unwrap());
+
+        // hacked
+        for _ in 0..10000 {
+            self.match_instr(self.ram[self.pc as usize]);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ mod m6502;
 use m6502::M6502;
 
 fn main() {
-    let cpu = M6502 { pc: 0, ac: 0, x: 0, y: 0, sr: 0, sp: 0 };
+    let mut cpu = M6502 { pc: 0, ac: 0, x: 0, y: 0, sr: 0, sp: 0, ram: [0; 256]};
 
     println!("{:?}", cpu);
-    println!("{:?}", cpu.match_instr(0x2a));
+
+    cpu.run("A9004828A90008688D8713A9FF4828A90008688D861360");
+
+    println!("{:?}", cpu);
 }
